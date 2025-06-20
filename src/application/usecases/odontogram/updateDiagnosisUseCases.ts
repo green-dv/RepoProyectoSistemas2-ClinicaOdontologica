4ed923dff65e090ccd20d7653bc1355bc22bbcc7
green-dv <jip2027354@est.univalle.edu>
2025-06-20 17:47:37 -0400

    Correccion de params en api 12

--- src/application/usecases/odontogram/updateDiagnosisUseCases.ts
+++ src/application/usecases/odontogram/updateDiagnosisUseCases.ts
@@ -4,7 +4,7 @@ import { Diagnosis } from "@/domain/entities/Diagnosis";
 export class UpdateDiagnosisUseCases {
     constructor(private odontogramRepository: OdontogramaRepository) {}
 
-    async execute(diagnosis: Diagnosis): Promise<Diagnosis[] | null> {
+    async execute(diagnosis: Diagnosis): Promise<Diagnosis | null> {
         return this.odontogramRepository.updateDiagnosis(diagnosis);
     }
 }
