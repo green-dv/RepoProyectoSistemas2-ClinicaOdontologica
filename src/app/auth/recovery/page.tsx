17352d1a5d240617eddf83551a0613df7066a214
green-dv <jip2027354@est.univalle.edu>
2025-06-22 15:51:43 -0400

    mas y mas correcciones pequenas 5

--- src/app/auth/recovery/page.tsx
+++ src/app/auth/recovery/page.tsx
@@ -1,11 +1,18 @@
 'use client';
-import PasswordRecoveryComponent from "@/components/login/passwordRecovery";
+
+import PasswordRecoveryComponent from '@/components/login/passwordRecovery';
 import { Suspense } from 'react';
 
-export default function RecoveryPage() {
+export default function RecoveryPage({ searchParams }: { searchParams: Record<string, string> }) {
+  const email = searchParams.email || '';
+  const passwordRecovery = searchParams.passwordRecovery === 'true';
+
   return (
-   <Suspense fallback={<div>Cargando...</div>}>
-      <PasswordRecoveryComponent />
+    <Suspense fallback={<div>Cargando...</div>}>
+      <PasswordRecoveryComponent
+        emailParam={email}
+        isPasswordRecoveryParam={passwordRecovery}
+      />
     </Suspense>
   );
 }
