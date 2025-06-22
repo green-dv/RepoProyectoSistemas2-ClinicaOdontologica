e0beaabfbe455ee42db19796a191f539d9b33a0d
green-dv <jip2027354@est.univalle.edu>
2025-06-22 16:12:10 -0400

    mas y mas correcciones pequenas 10

--- src/app/auth/recovery/page.tsx
+++ src/app/auth/recovery/page.tsx
@@ -1,11 +1,12 @@
+'use client';
 import { Suspense } from 'react';
 import PasswordRecoveryComponent from '@/components/login/passwordRecovery';
 
-type Props = {
-  searchParams?: { [key: string]: string | string[] | undefined };
+type PageProps = {
+  searchParams?: Record<string, string | string[] | undefined>;
 };
 
-export default function RecoveryPage({ searchParams }: Props) {
+export default function RecoveryPage({ searchParams }: PageProps) {
   const email = typeof searchParams?.email === 'string' ? searchParams.email : '';
   const isPasswordRecovery = searchParams?.passwordRecovery === 'true';
 
